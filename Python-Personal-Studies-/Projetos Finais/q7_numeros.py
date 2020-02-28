55ef8d5fde05ad931aac3729c0b5ce193533837b
Lucas Noleto <lucas_noleto@outlook.com>
2020-02-28 06:09:05 -0300

    Aula 3 de widget e exercicio.

---
+++ Python-Personal-Studies-/Projetos Finais/q7_numeros.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 28 05:52:22 2020
+
+@author: lucas
+"""
+
+
+valor_compra = float(input("Digite o valor da compra"))
+pagamento =  float(input ("Insira valor pago"))
+troco = pagamento - valor_compra
+
+print ("Troco: %1.2f" %(troco)) 
